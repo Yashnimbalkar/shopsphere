d829f74937a15f1c2e1138b09291080c0bca179d
Yash NImbalkar <nimbalkaryash99@gmail.com>
2026-07-16 14:12:17 +0530

    feat: add  useAuth.js fiel

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { useNavigate, Link } from 'react-router-dom'
 import { loginUser } from '../services/authService'
-import { useAuth } from '../context/AuthContext'
+import { useAuth } from '../context/useAuth'
 
 function Login() {
   const [formData, setFormData] = useState({ email: '', password: '' })
